7d3e0334ffa31ffba23af4e3d89e6174ba4df13f
Gnanapriya <117845393+priyabaski5@users.noreply.github.com>
2025-01-06 14:37:17 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors());
+const allowedOrigins = ['https://ornate-monstera-538874.netlify.app'];
+app.use(cors({
+    origin: allowedOrigins
+}));
 
 mongoose
     .connect(process.env.Mongodb_URL)
